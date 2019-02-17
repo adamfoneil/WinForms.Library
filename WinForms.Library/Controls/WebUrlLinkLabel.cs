b9f9c0cd0b6dd93e7e750d4eddb2c6b801eae9e8
Adam O'Neil <adamosoftware@gmail.com>
2019-02-17 13:52:44 -0500

    added WebUrlLinkLabel

---
+++ WinForms.Library/Controls/WebUrlLinkLabel.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WinForms.Library.Controls
+{
+	public class WebUrlLinkLabel : LinkLabel
+	{
+		public string Url { get; set; }
+
+		protected override void OnLinkClicked(LinkLabelLinkClickedEventArgs e)
+		{
+			base.OnLinkClicked(e);
+			Process.Start(Url);
+		}
+	}
+}
