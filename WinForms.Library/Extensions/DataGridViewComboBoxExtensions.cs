e86cf89384ad5fa653e91a0379ef966a99c73ebb
Adam O'Neil <adamosoftware@gmail.com>
2019-12-12 15:59:21 -0500

    versiom 1.0.39 added DataGridViewComboBoxExtensions Fill method for enums

---
+++ WinForms.Library/Extensions/DataGridViewComboBoxExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using WinForms.Library.Models;
+
+namespace WinForms.Library.Extensions
+{
+    public static class DataGridViewComboBoxExtensions
+    {
+        public static void Fill<TEnum>(this DataGridViewComboBoxColumn column)
+        {
+            var names = Enum.GetNames(typeof(TEnum));
+            var values = Enum.GetValues(typeof(TEnum)).OfType<TEnum>();
+
+            column.Items.Clear();
+
+            int index = 0;
+            values.ToList().ForEach((e) =>
+            {
+                column.Items.Add(new ListItem<TEnum>(e, names[index]));
+                index++;
+            });
+        }
+    }
+}
